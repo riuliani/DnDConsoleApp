7d2599480977b8b1ccd11761c029cf3535608aaf
Raffaele Iuliani <raff.iuliani@gmail.com>
2022-05-30 22:45:47 -0400

    Changed class name to Player.

---
+++ DnDApplication/DnDCore/Player.cs
@@ -0,0 +1,25 @@
+﻿using DnDCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCore
+{
+    public class Player : IPlayer
+    {
+        public string Name { get; set; } = "";
+        public int Health { get; set; }
+        public int DamageDone { get; set; }
+        public int DamageTaken { get; set; }
+
+        public Player(string name, int health)
+        {
+            Name = name;
+            Health = health;
+            DamageDone = 0;
+            DamageTaken = 0;
+        }
+    }
+}
