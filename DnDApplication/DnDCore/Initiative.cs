51190f55d2aed7e57026f5b77bd5e88904d04697
Raffaele Iuliani <raff.iuliani@gmail.com>
2022-06-04 07:08:46 -0400

    Added initiative class.

---
+++ DnDApplication/DnDCore/Initiative.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCore
+{
+    public class Initiative
+    {        
+        public void AddRolesTolist(int initiativeRoll)
+        {
+            var initRollList = new List<int>();
+            initRollList.Add(initiativeRoll);
+        }
+
+        public void DisplayOrderFromLargestToSmallest(List<string> initRollList)
+        {
+            initRollList.Reverse();
+            foreach (var initRoll in initRollList)
+            {
+                Console.WriteLine($"{initRoll}");
+            }
+        }
+    }
+}
