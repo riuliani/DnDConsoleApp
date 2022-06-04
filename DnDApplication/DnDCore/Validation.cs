acb6085a942f94cf3dfc0c7297b95286ce7b0d5b
Raffaele Iuliani <raff.iuliani@gmail.com>
2022-06-04 06:21:48 -0400

    Added new class for validation.

---
+++ DnDApplication/DnDCore/Validation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCore
+{
+    public static class Validation
+    {
+        public static bool CheckUserInput(string result)
+        {
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int ConvertStringToInt(bool result, string userInput)
+        {
+            var newNumber = 0;
+            if (result == true)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                newNumber = Convert.ToInt32(userInput, newNumber);
+                return newNumber;
+            }
+        }
+    }
+}
