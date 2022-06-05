8321b88ffef9ba786fcb0daaf1c0752fdbb7845e
Raffaele Iuliani <raff.iuliani@gmail.com>
2022-06-05 06:31:23 -0400

    Added logic for testing.

--- DnDApplication/DnDCore/Program.cs
+++ DnDApplication/DnDCore/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DnDCore;
+
 Console.WriteLine("Hello, World!");
+var herelian = new Herelian("Herelian", 105);
+
+var damage = Combat.AttackEnemy(herelian);
+herelian.AddDamageDone(damage);
+Console.WriteLine($"Total damage done by {herelian.Name}: {herelian.DamageDone}");
