116c782c4242eab607520cb19a9b0c701987e3bc
Raffaele Iuliani <raff.iuliani@gmail.com>
2022-06-05 07:04:34 -0400

    Added second unit test.

--- DnDApplication/DnDCore.Tests/ValidationTests.cs
+++ DnDApplication/DnDCore.Tests/ValidationTests.cs
@@ -28,5 +28,31 @@ namespace DnDCore.Tests
             Assert.Pass();
         }
         #endregion
+
+        #region Convert to int Tests
+        [Test]
+        [TestCase(true,"",0)]
+        public void Testing_ConverToString_Method_If(bool isValid, string userInput, int expectedResult)
+        {
+            var result = Validation.ConvertStringToInt(isValid, userInput);
+
+            Assert.IsTrue(result == expectedResult);
+            Assert.Pass();
+        }
+
+        [Test]
+        [TestCase(false,"0",0)]
+        [TestCase(false,"10",10)]
+        [TestCase(false,"15",15)]
+        [TestCase(false,"266",266)]
+        [TestCase(false,"9487",9487)]
+        public void Testing_ConverToString_Method_Else(bool isValid,string userInput, int expectedResult)
+        {
+            var result = Validation.ConvertStringToInt(isValid, userInput);
+
+            Assert.IsTrue(result == expectedResult);
+            Assert.Pass();
+        }
+        #endregion
     }
 }
